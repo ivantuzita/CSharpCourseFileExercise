7c60cd6524b5e08868357b8955a25c94d9905726
Ivan Tuzita <ivantuzita@gmail.com>
2023-04-03 12:53:26 -0300

    done exercise

---
+++ CSharpCourseFileExercise/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpCourseFileExercise.Entities {
+    class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity) {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double totalPrice() {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ CSharpCourseFileExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using CSharpCourseFileExercise.Entities;
+using System.Globalization;
+
+string sourcePath = @"D:\programming\C#\CSharpCourseFileExercise\CSharpCourseFileExercise\Files\sourcefile.csv";
+string targetPath = @"D:\programming\C#\CSharpCourseFileExercise\CSharpCourseFileExercise\Files\summary.csv";
+List<Product> products = new List<Product>();
+
+try {
+    using (FileStream fs = new FileStream(sourcePath, FileMode.Open)) {
+        using (StreamReader sr = new StreamReader(fs)) {
+            while (!sr.EndOfStream) {
+                string[] line = sr.ReadLine().Split(',');
+                Product product = new(line[0], double.Parse(line[1], CultureInfo.InvariantCulture), int.Parse(line[2]));
+                products.Add(product);
+            }
+        }
+    }
+}
+catch {
+    Console.WriteLine("An error occured while reading file!");
+}
+
+try {
+    using (StreamWriter sw = File.AppendText(targetPath)) {
+        foreach (Product product in products) {
+            sw.WriteLine($"{product.Name},{product.totalPrice()}");
+        }
+        Console.WriteLine("Success!");
+    }
+}
+catch {
+    Console.WriteLine("An error occured while writing to target file!");
+}
